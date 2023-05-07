7bba01dfe50b6c796aaee65033702c0fceb69ad5
Sinha5714 <>
2023-05-07 19:12:28 +0000

    Comment: Create comment model and migrate it to database

--- blog/models.py
+++ blog/models.py
@@ -29,3 +29,22 @@ class HumanitasPost(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+
+class Comment(models.Model):
+    """
+    Model fo comments from users
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    humanitas_post = models.ForeignKey(
+        HumanitasPost, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=60)
+    content = models.TextField(max_length=300)
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.author.username + '|commented on ' + self.humanitaspost.title
