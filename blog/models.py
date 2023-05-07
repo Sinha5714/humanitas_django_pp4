4b77bb62550bd8bcb2125f2a4fca4e3d00049aac
Sinha5714 <>
2023-05-07 18:51:51 +0000

    HumanitasPost: Create humanitaspost model and migrate to database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class HumanitasPost(models.Model):
+    """
+    HumanitasPost model used for each blog posted by users
+    """
+
+    title = models.CharField(max_length=200, unique=True)
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="humanitas_posts")
+    slug = models.SlugField(max_length=100, unique=True)
+    body = models.TextField()
+    cover_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.creator)
+
+    def number_of_likes(self):
+        return self.likes.count()
