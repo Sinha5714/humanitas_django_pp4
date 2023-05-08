cfd357379f25705e09a544d1c4be9616125e535f
Sinha5714 <>
2023-05-08 18:06:49 +0000

    Add alerts from django.contrib.messages in base.html

--- humanitas/settings.py
+++ humanitas/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import django.contrib.messages import constants as messages
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -68,7 +68,6 @@ MESSAGE_TAGS = {
     messages.SUCCESS: 'alert-success',
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
-
 }
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
