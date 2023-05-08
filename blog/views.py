71b321efa968f603e156875db0833a244eecdce2
Sinha5714 <>
2023-05-08 05:07:29 +0000

    HumanitasPostView: Create class HumanitasPostView to view a list of all posts and add humanitas-post.html file

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import HumanitasPost
 
 # Create your views here.
+
+
+class HumanitasPostView(ListView):
+    """
+    A class view to view a list of all posts
+    """
+    model = HumanitasPost
+    context_object_name = 'humanitas_post'
+    template_name = 'blog/humanitas-blog.html'
+    paginate_by = 8
