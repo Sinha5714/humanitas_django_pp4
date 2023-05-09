630c26e7473b474dd0493397c7797b86d17c88ea
Sinha5714 <>
2023-05-09 07:10:43 +0000

    Add contact function in views.py to render contact.html template

--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView
+from django.contrib import messages
+from django.core.mail import send_mail
+from .forms import ContactUsForm
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    """
+    view to render contact page
+    """
+    form = ContactUsForm()
+    return render(request, 'contact.html')
