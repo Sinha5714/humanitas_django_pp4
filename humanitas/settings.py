cd871f7d78db0997cd77f09e5bcd4c9e91f09603
Sinha5714 <>
2023-05-10 11:16:28 +0000

    Add new local allowed host in settings.py

--- humanitas/settings.py
+++ humanitas/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sinha5714-humanitas-djan-2cueq8qiak.us2.codeanyapp.com',
+ALLOWED_HOSTS = ['8000-sinha5714-humanitas-djan-giegkq4ixp.us2.codeanyapp.com',
                  'humanitas-django-pp4.herokuapp.com', 'localhost']
 
 
