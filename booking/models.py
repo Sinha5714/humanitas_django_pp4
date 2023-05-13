9f4a40ac466b00c1e9268820f998da40242e31ff
Sinha5714 <>
2023-05-13 17:35:19 +0000

    Model Booking: Create Booking model for user to book a time for call with admin

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import date
+from django.conf import settings
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+
+    TIMEBLOCK_CHOICES = (
+        ("A", "10:00-11:00"),
+        ("B", "11:00-12:00"),
+        ("C", "12:00-13:00"),
+        ("D", "13:00-14:00"),
+        ("E", "14:00-15:00"),
+        ("F", "15:00-16:00"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+    timeblock = models.CharField(
+        max_length=10, choices=TIMEBLOCK_CHOICES, default="A")
+    helptype = models.CharField(max_length=50, default="")
