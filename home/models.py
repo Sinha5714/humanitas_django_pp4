1c7c9127f7b61813f92fa6303836c4ff9d9abd26
Sinha5714 <>
2023-05-13 10:22:16 +0000

    Create Profile class model for profile of the user

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    """
+    Class model for profile of the user
+    """
+	user=models.OneToOneField(User, on_delete=models.CASCADE)
+	profile_image=models.CloudinaryField('image', default='profile_pic')
+
+	def __str__(self):
+		return f'{self.user.username} Profile'
+
+	def save(self,*args,**kwargs):
+		super().save(*args,**kwargs)
