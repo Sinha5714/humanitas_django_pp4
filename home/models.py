37fb173843744f1d96901b972cc3adb8659ed90d
Sinha5714 <>
2023-05-13 14:58:32 +0000

    Profile.html: Create profile.html for content of profile of user

--- home/models.py
+++ home/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
 
 class Profile(models.Model):
     """
     Class model for profile of the user
     """
-	user=models.OneToOneField(User, on_delete=models.CASCADE)
-	profile_image=models.CloudinaryField('image', default='profile_pic')
+
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	profile_image = models.CloudinaryField('image', default='profile_pic')
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
