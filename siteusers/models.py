d5ec3a78584cfc035d4793ab911e5739a33b7fae
Sinha5714 <>
2023-05-16 07:40:56 +0000

    Profile Model: Create Profile model which conatins user and profile_image for registered users

--- siteusers/models.py
+++ siteusers/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
-
+from django.urls import reverse
+from cloudinary.models import CloudinaryField
 # Create your models here.
+
+
+class Profile(models.Model):
+    """
+    Profile model used as an account
+    for each registered user
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
--- stories/admin.py
+++ stories/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import HumanitasPost, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import HumanitasPost, Comment
 
 
 @admin.register(HumanitasPost)
