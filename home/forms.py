bebc19ed9327bb45c20b02f3009f1d54e226a54f
Sinha5714 <>
2023-05-20 10:11:31 +0000

    Add ContactUsForm for user to send message using this form in home/forms.py

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,18 @@ from .models import Profile, Contact
 from django import forms
 
 
+class ContactUsForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = Contact
+        fields = (
+            'name',
+            'email',
+            'message')
+
 
 class ProfileForm(forms.ModelForm):
     """
