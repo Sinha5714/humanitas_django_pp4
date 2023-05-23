e5326a37cd9782dab0f49b257c5ebc90d06e37c7
Sinha5714 <>
2023-05-23 07:30:53 +0000

    base.html: Add bootstrap 5 CDN links for navbar dropdown links to work and change format of navbar for better UI

--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
-setTimeout(function () {
-    let messages = document.getElementByID('msg');
-    let alert = new bootstrap.Alert(messages);
-    alert.close();
-}, 3000);
+$(document).ready(function () {
+    setTimeout(function () {
+        let messages = document.getElementById("msg");
+        let alert = new bootstrap.Alert(messages);
+        alert.close();
+    }, 3000);
+});
